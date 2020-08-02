63cb1f3c1cac2a0b78de5e2dd9e465b87ba58a22
Bhuvan-Agarwal <bhuvangarg123@gmail.com>
2020-08-02 19:04:34 +0530

    Design HashSet (Added)

---
+++ Day 2 (2nd August,2020)/Solution.java
@@ -0,0 +1,55 @@
+class MyHashSet {
+    
+    private int numBuckets = 15000;
+    List<Integer>[] buckets;
+    
+    private int hash_function(int key){
+        return key % numBuckets;
+    }
+
+    /** Initialize your data structure here. */
+    public MyHashSet() {
+        buckets = new LinkedList[numBuckets];
+    }
+    
+    public void add(int key) {
+        int i = hash_function(key);
+        
+        // If idx is not initialized , then create new LinkedList there
+        if(buckets[i] == null)
+            buckets[i] = new LinkedList<>();
+        
+        //  If the key is not present in that linkedList then we need to add it
+        if(buckets[i].indexOf(key) == -1)
+            buckets[i].add(key);
+    }
+    
+    public void remove(int key) {
+        int i = hash_function(key);
+        
+        // If idx is null ,  then the data is not present
+        if(buckets[i] == null) 
+            return;
+        
+              // If the key is present in the list , then it will definitely give an index position , so we need to remove that particular index location of our array
+        if(buckets[i].indexOf(key) != -1) 
+            buckets[i].remove(buckets[i].indexOf(key));
+    }
+    
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        int i = hash_function(key);
+        
+        if(buckets[i] == null || buckets[i].indexOf(key) == -1) 
+            return false;
+        return true;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
