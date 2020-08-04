238ede4978855116c1f1d57b16e4f19f34f772e0
Bhuvan-Agarwal <bhuvangarg123@gmail.com>
2020-08-04 16:02:51 +0530

    Power of Four

---
+++ Day 4 (4th August,2020)/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPowerOfFour(int num) {
+        if(num==0)
+            return false;
+        while(num!=1)
+        {
+            if(num%4==0)
+                num /= 4;
+            else
+                return false;
+        }
+      
+        return true;
+    }
+}
