1d068ac3c34175e413dcab8396359766c8c58544
Bhuvan-Agarwal <bhuvangarg123@gmail.com>
2020-08-15 11:32:52 +0530

    Kth Missing Positive Number Added

---
+++ Extra Questions Solved/Kth Missing Positive Number (Important)/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i:arr)
+            set.add(i);
+        
+        int n = 1;
+        while(k!=0)
+        {
+            if(!set.contains(n))
+                k--;
+            n++;
+        }
+        
+        return n-1;
+    }
+}
