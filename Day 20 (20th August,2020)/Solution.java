87642805962346510fb533fbf610034e4e8d9065
Bhuvan-Agarwal <bhuvangarg123@gmail.com>
2020-08-21 10:37:31 +0530

    Reorder List

---
+++ Day 20 (20th August,2020)/Solution.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+   public void reorderList(ListNode head) {
+        if(head == null || head.next == null) return;
+        
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode prev = null, curr = slow, tmp;
+        while(curr != null){
+            tmp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = tmp;
+        }
+        
+        ListNode n1 = head, n2 = prev;
+        while(n2.next != null){
+            tmp = n1.next;
+            n1.next = n2;
+            n1 = tmp;
+            
+            tmp = n2.next;
+            n2.next = n1;
+            n2 = tmp;
+        }
+    }
+}
---
+++ Day 20 (20th August,2020)/readme.txt
@@ -0,0 +1,4 @@
+This Code is being testes with 13 TestCases...
+
+The Link to the Problem is =>	
+	https://leetcode.com/problems/reorder-list/
